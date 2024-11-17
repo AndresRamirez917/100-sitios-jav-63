e49fe7ffb3ea9fc1ee434360ba43424a31d7235a
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-17 15:02:53 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 async function getData(){
-    const result = await fetch('https://randomuser.me/api?results=8')
+    const result = await fetch('https://randomuser.me/api?results=6')
     const character = await result.json();
     console.log(character)
     character.results.forEach(element => {
@@ -18,4 +18,32 @@ async function getData(){
     });
 }
 
+async function getData2(){
+    const result = await fetch('https://rickandmortyapi.com/api/character');
+    const character = await result.json();
+    console.log(character);
+    const jsonArr = character.results.map(elemento => Object.entries(elemento));
+    console.log(jsonArr)
+    const jsonArrSliced = jsonArr.slice(0,4);
+    character.results.forEach(element => {
+        const randData = (min, max) => Math.floor(Math.random()*(max - min + 1) + min);
+        const randIndex = randData(1, jsonArr.length);
+        for(i = 0; i < jsonArrSliced.length; i++){
+            if(element.id == i){
+                const box = document.createRange().createContextualFragment(`
+                    
+                    <div class="boxx-1">
+                        <img src="${jsonArr[randIndex][8][1]}" alt="">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem perferendis eum excepturi ea itaque illum architecto placeat dolore enim doloremque?</p>
+                    </div>
+                    
+                    `)
+                    const services_row = document.querySelector('.services-row');
+                    services_row.append(box)
+            }
+        }
+    })
+}
+
 getData()
+getData2()
